9eadabe1fb5b008eede0f193d6b2dd25be9db4b1
sidvud98 <sidvud98@gmail.com>
2023-11-22 18:47:11 +0530

    copyright notice added

--- src/App.js
+++ src/App.js
@@ -13,3 +13,26 @@ const App = () => {
 }
 
 export default React.memo(App);
+
+
+
+
+
+
+
+
+
+
+
+/*
+  COPYRIGHT NOTICE:
+
+  This project and its contents are confidential and proprietary to Sai Siddharth Vudikavalasa. 
+  They are intended solely for the use of the individual or entity to whom they are addressed.
+  All rights reserved. Unauthorized use or distribution, in whole or in part, is prohibited. 
+  Use of this project for commercial purposes is not permitted without prior consent from the author.
+
+  Sai Siddharth Vudikavalasa
+  22nd November 2023
+  sidvud98@gmail.com
+*/
--- src/components/DetailedPane.js
+++ src/components/DetailedPane.js
@@ -71,3 +71,23 @@ const DetailedPane = () => {
 
 
 export default React.memo(DetailedPane)
+
+
+
+
+
+
+
+
+/*
+  COPYRIGHT NOTICE:
+
+  This project and its contents are confidential and proprietary to Sai Siddharth Vudikavalasa. 
+  They are intended solely for the use of the individual or entity to whom they are addressed.
+  All rights reserved. Unauthorized use or distribution, in whole or in part, is prohibited. 
+  Use of this project for commercial purposes is not permitted without prior consent from the author.
+
+  Sai Siddharth Vudikavalasa
+  22nd November 2023
+  sidvud98@gmail.com
+*/
